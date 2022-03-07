ec72e6eb3318f6385eb9775dfbea97375a6c417e
大海团队 <mp@sea.team>
2022-03-07 15:07:14 +0800

    roles

--- pages/channel-manage/index.tsx
+++ pages/channel-manage/index.tsx
@@ -2,7 +2,18 @@ import type { NextPage } from 'next'
 import React, { useEffect, useState } from 'react'
 import { useStore } from '../../assets/js/store'
 import { SeaSwitch, SeaRole, SeaIcon, SeaChannel } from '../../components'
-import { Button, IconButton, MenuItem, Select, SelectChangeEvent, Paper } from '@mui/material'
+import {
+  Button,
+  IconButton,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+  Paper,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  Typography,
+} from '@mui/material'
 import api from '../../assets/js/api'
 
 const Page: NextPage = () => {
@@ -17,7 +28,6 @@ const Page: NextPage = () => {
   const bindSwitch = (event: React.ChangeEvent<HTMLInputElement>) => {
     console.log('🌊', event.target.checked)
   }
-
   // channel select
   const [channel, setChannel] = React.useState('UniPass')
   const bindChannel = (event: SelectChangeEvent) => {
@@ -44,123 +54,88 @@ const Page: NextPage = () => {
   ])
   return (
     <div id="page-channel-manage">
-      <div className="sea-box-one">
-        <div className="info">
-          <h3>Channel</h3>
-          <SeaChannel name="UniPass" />
-          <SeaSwitch className="switch" onChange={bindSwitch} defaultChecked={true} />
-          <IconButton sx={{ marginLeft: '8px' }}>
-            <SeaIcon icon="ic:round-keyboard-arrow-up" />
-          </IconButton>
-        </div>
-        <div className="tip-box">
-          <div className="one">
-            <SeaIcon icon="icon-park-outline:message-one"></SeaIcon>
-            <SeaRole color="#c4505e" text="UP Lv4" />
-            <SeaRole color="#e9c0a0" text="UP Lv3" />
-          </div>
-          <div className="one">
-            <SeaIcon icon="icon-park-outline:message-privacy"></SeaIcon>
-            <SeaRole color="#4fab9f" text="UP Lv1" />
-            <SeaRole color="#3b7669" text="UP Lv2" />
-          </div>
-        </div>
-        <h3>Set Channel</h3>
-        <Select className="select" size="small" value={channel} onChange={bindChannel}>
-          <MenuItem value="UniPass"># UniPass</MenuItem>
-          <MenuItem value="Aven"># Aven</MenuItem>
-        </Select>
-        <h3>Message manage</h3>
-        <Paper className="sea-paper" elevation={12}>
-          <div className="view-channel">
-            <h4>View channel</h4>
-            <h5>Allows members to view this channel.</h5>
-            <div className="sea-operation-box">
-              {viewChannel.map((e, i) => {
-                return <SeaRole key={i} color={e.color} text={e.lv} />
-              })}
-              <div className="operation">
-                <IconButton>
-                  <SeaIcon icon="fluent:add-circle-16-regular"></SeaIcon>
-                </IconButton>
-                <IconButton>
-                  <SeaIcon icon="fluent:subtract-circle-16-regular"></SeaIcon>
-                </IconButton>
+      <Accordion className="sea-box-one">
+        <AccordionSummary
+          expandIcon={<SeaIcon sx={{ fontSize: '32px' }} icon="ic:round-expand-more" />}
+        >
+          <Typography className="info">
+            <div className="title">
+              <div className="sea-h3">Channel</div>
+              <SeaChannel name="UniPass" />
+              <SeaSwitch
+                className="switch"
+                onClick={(event) => {
+                  event.stopPropagation()
+                }}
+                onChange={bindSwitch}
+                defaultChecked={true}
+              />
+            </div>
+            <div className="tip-box">
+              <div className="one">
+                <SeaIcon icon="icon-park-outline:message-one"></SeaIcon>
+                <SeaRole color="#c4505e" text="UP Lv4" />
+                <SeaRole color="#e9c0a0" text="UP Lv3" />
+              </div>
+              <div className="one">
+                <SeaIcon icon="icon-park-outline:message-privacy"></SeaIcon>
+                <SeaRole color="#4fab9f" text="UP Lv1" />
+                <SeaRole color="#3b7669" text="UP Lv2" />
               </div>
             </div>
-          </div>
-        </Paper>
-        <Paper className="sea-paper" elevation={12}>
-          <div className="send-message">
-            <h4>Send message</h4>
-            <h5>Allow those members to publish their own message.</h5>
-            <div className="sea-operation-box">
-              <SeaRole color="#4fab9f" text="UP Lv1" />
-              <SeaRole color="#3b7669" text="UP Lv2" />
-              <div className="operation">
-                <IconButton>
-                  <SeaIcon icon="fluent:add-circle-16-regular"></SeaIcon>
-                </IconButton>
-                <IconButton>
-                  <SeaIcon icon="fluent:subtract-circle-16-regular"></SeaIcon>
-                </IconButton>
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <div className="sea-h3">Set Channel</div>
+          <Select className="select" size="small" value={channel} onChange={bindChannel}>
+            <MenuItem value="UniPass"># UniPass</MenuItem>
+            <MenuItem value="Aven"># Aven</MenuItem>
+          </Select>
+          <div className="sea-h3">Message manage</div>
+          <Paper className="sea-paper" elevation={12}>
+            <div className="view-channel">
+              <h4>View channel</h4>
+              <h5>Allows members to view this channel.</h5>
+              <div className="sea-operation-box">
+                {viewChannel.map((e, i) => {
+                  return <SeaRole key={i} color={e.color} text={e.lv} />
+                })}
+                <div className="operation">
+                  <IconButton>
+                    <SeaIcon icon="fluent:add-circle-16-regular"></SeaIcon>
+                  </IconButton>
+                  <IconButton>
+                    <SeaIcon icon="fluent:subtract-circle-16-regular"></SeaIcon>
+                  </IconButton>
+                </div>
               </div>
             </div>
+          </Paper>
+          <Paper className="sea-paper" elevation={12}>
+            <div className="send-message">
+              <h4>Send message</h4>
+              <h5>Allow those members to publish their own message.</h5>
+              <div className="sea-operation-box">
+                <SeaRole color="#4fab9f" text="UP Lv1" />
+                <SeaRole color="#3b7669" text="UP Lv2" />
+                <div className="operation">
+                  <IconButton>
+                    <SeaIcon icon="fluent:add-circle-16-regular"></SeaIcon>
+                  </IconButton>
+                  <IconButton>
+                    <SeaIcon icon="fluent:subtract-circle-16-regular"></SeaIcon>
+                  </IconButton>
+                </div>
+              </div>
+            </div>
+          </Paper>
+          <div className="save">
+            <Button className="submit" variant="contained" color="secondary">
+              Save
+            </Button>
           </div>
-        </Paper>
-        <div className="save">
-          <Button className="submit" variant="contained" color="secondary">
-            Save
-          </Button>
-        </div>
-      </div>
-      <div className="sea-box-one">
-        <div className="info">
-          <h3>Channel</h3>
-          <SeaChannel name="UniPass-VIP" />
-          <SeaSwitch className="switch" onChange={bindSwitch} defaultChecked={true} />
-          <IconButton sx={{ marginLeft: '8px' }}>
-            <SeaIcon icon="ic:round-keyboard-arrow-up" sx={{ transform: 'rotate(180deg)' }} />
-          </IconButton>
-        </div>
-        <div className="tip-box">
-          <div className="one">
-            <SeaIcon icon="icon-park-outline:message-one"></SeaIcon>
-            <SeaRole color="#c4505e" text="UP Lv4" />
-            <SeaRole color="#e9c0a0" text="UP Lv3" />
-          </div>
-          <div className="one">
-            <SeaIcon icon="icon-park-outline:message-privacy"></SeaIcon>
-            <SeaRole color="#4fab9f" text="UP Lv1" />
-            <SeaRole color="#3b7669" text="UP Lv2" />
-          </div>
-        </div>
-      </div>
-      <div className="sea-box-one">
-        <div className="info">
-          <h3>Channel</h3>
-          <SeaChannel name="UniPass-VIP2" />
-          <SeaSwitch className="switch" onChange={bindSwitch} defaultChecked={false} />
-          <IconButton sx={{ marginLeft: '8px' }}>
-            <SeaIcon icon="ic:round-keyboard-arrow-up" sx={{ transform: 'rotate(180deg)' }} />
-          </IconButton>
-        </div>
-        <div className="tip-box">
-          <div className="one">
-            <SeaIcon icon="icon-park-outline:message-one"></SeaIcon>
-            <SeaRole color="#c4505e" text="UP Lv4" />
-            <SeaRole color="#e9c0a0" text="UP Lv3" />
-          </div>
-          <div className="one">
-            <SeaIcon icon="icon-park-outline:message-privacy"></SeaIcon>
-            <SeaRole color="#4fab9f" text="UP Lv1" />
-            <SeaRole color="#3b7669" text="UP Lv2" />
-          </div>
-        </div>
-      </div>
-      <div className="sea-new-box">
-        <Button>+ Add a new channel manage</Button>
-      </div>
+        </AccordionDetails>
+      </Accordion>
     </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,18 @@
 import { message } from 'antd'
 import type { NextPage } from 'next'
 import React, { useEffect, useState } from 'react'
-import { Button, IconButton, MenuItem, Select, TextField, Paper } from '@mui/material'
+import {
+  Button,
+  IconButton,
+  MenuItem,
+  Select,
+  TextField,
+  Paper,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  Typography,
+} from '@mui/material'
 import UP, { UPAuthMessage } from 'up-core-test'
 import { SeaSwitch, SeaRole, SeaIcon } from '../components'
 import { useStore } from '../assets/js/store'
@@ -27,7 +38,6 @@ const Page: NextPage = () => {
       }
     })
   }, [state.server])
-
   const bindSave = async (role: Role) => {
     console.log('role', role)
     const account = state.account
@@ -103,154 +113,168 @@ const Page: NextPage = () => {
       setRoles([...roles])
     }
   }
+  const bindOpen = (event: React.ChangeEvent<HTMLInputElement>, i: number) => {
+    const open = event.target.checked
+    roles[i].open = open
+  }
   return (
     <div id="page-index">
       {roles.map((e: Role, i) => {
         return (
-          <div key={i} className="sea-box-one">
-            <div className="info">
-              <h3>Role</h3>
-              <SeaRole color={formatColor(e.color)} text={e.name} />
-              <SeaSwitch
-                className="switch"
-                onChange={(event, open) => (roles[i].open = open)}
-                defaultChecked={e.open}
-              />
-              <IconButton sx={{ marginLeft: '8px' }}>
-                <SeaIcon icon="ic:round-keyboard-arrow-up" />
-              </IconButton>
-            </div>
-            <div className="tip-box">
-              <ul>
-                {e.uniPassRequirement.map((e, i) => (
-                  <li key={i}>{uniPassRequirementFormat(e)}</li>
-                ))}
-                {e.assetRequirement.map((e, i) => (
-                  <li key={i}>{assetRequirementFormat(e)}</li>
-                ))}
-              </ul>
-            </div>
-            <h3>Set Role</h3>
-            <Select size="small" defaultValue="OG User" disabled>
-              <MenuItem value="OG User">
-                <SeaRole color="#c4505e" text="OG User" />
-              </MenuItem>
-              <MenuItem value="UP Lv1">
-                <SeaRole color="#4fab9f" text="UP Lv1" />
-              </MenuItem>
-              <MenuItem value="UP Lv2">
-                <SeaRole color="#3b7669" text="UP Lv2" />
-              </MenuItem>
-            </Select>
-            <h3>Set Requirement</h3>
-            {e.uniPassRequirement.length > 0 &&
-              e.uniPassRequirement.map((e, i2) => (
-                <Paper key={i2} className="sea-paper role-one" elevation={12}>
-                  <div className="left">
-                    <h4>UniPass requirement</h4>
-                    <div className="sea-operation-box">
-                      <Select size="small" defaultValue="Level" disabled>
-                        <MenuItem value="Level">Level</MenuItem>
-                      </Select>
-                      <Select size="small" defaultValue={e.level.range}>
-                        <MenuItem value={0}>{'≥'}</MenuItem>
-                        <MenuItem value={1}>{'≤'}</MenuItem>
-                        <MenuItem value={2}>{'='}</MenuItem>
-                      </Select>
-                      <Select
-                        size="small"
-                        defaultValue={e.level.level}
-                        onChange={(event) => (e.level.level = Number(event.target.value))}
+          <Accordion key={i} className="sea-box-one">
+            <AccordionSummary
+              expandIcon={<SeaIcon sx={{ fontSize: '32px' }} icon="ic:round-expand-more" />}
+            >
+              <Typography className="info">
+                <div className="title">
+                  <div className="sea-h3">Role</div>
+                  <SeaRole color={formatColor(e.color)} text={e.name} />
+                  <SeaSwitch
+                    onClick={(event) => {
+                      event.stopPropagation()
+                    }}
+                    className="switch"
+                    onChange={(event) => bindOpen(event, i)}
+                    defaultChecked={e.open}
+                  />
+                </div>
+                <div className="tip-box">
+                  <ul>
+                    {e.uniPassRequirement.map((e, i) => (
+                      <li key={i}>{uniPassRequirementFormat(e)}</li>
+                    ))}
+                    {e.assetRequirement.map((e, i) => (
+                      <li key={i}>{assetRequirementFormat(e)}</li>
+                    ))}
+                  </ul>
+                </div>
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                <div className="sea-h3">Set Role</div>
+                <Select size="small" defaultValue="OG User" disabled>
+                  <MenuItem value="OG User">
+                    <SeaRole color="#c4505e" text="OG User" />
+                  </MenuItem>
+                  <MenuItem value="UP Lv1">
+                    <SeaRole color="#4fab9f" text="UP Lv1" />
+                  </MenuItem>
+                  <MenuItem value="UP Lv2">
+                    <SeaRole color="#3b7669" text="UP Lv2" />
+                  </MenuItem>
+                </Select>
+                <div className="sea-h3">Set Requirement</div>
+                {e.uniPassRequirement.length > 0 &&
+                  e.uniPassRequirement.map((e, i2) => (
+                    <Paper key={i2} className="sea-paper role-one" elevation={12}>
+                      <div className="left">
+                        <h4>UniPass requirement</h4>
+                        <div className="sea-operation-box">
+                          <Select size="small" defaultValue="Level" disabled>
+                            <MenuItem value="Level">Level</MenuItem>
+                          </Select>
+                          <Select size="small" defaultValue={e.level.range}>
+                            <MenuItem value={0}>{'≥'}</MenuItem>
+                            <MenuItem value={1}>{'≤'}</MenuItem>
+                            <MenuItem value={2}>{'='}</MenuItem>
+                          </Select>
+                          <Select
+                            size="small"
+                            defaultValue={e.level.level}
+                            onChange={(event) => (e.level.level = Number(event.target.value))}
+                          >
+                            {[0, 1, 2, 3, 4, 5, 6].map((lv) => (
+                              <MenuItem key={lv} value={lv}>
+                                Lv{lv}
+                              </MenuItem>
+                            ))}
+                          </Select>
+                        </div>
+                      </div>
+                      <IconButton
+                        className="right delete"
+                        aria-label="delete"
+                        color="error"
+                        onClick={() => bindDel('unipass', i, i2)}
                       >
-                        {[0, 1, 2, 3, 4, 5, 6].map((lv) => (
-                          <MenuItem key={lv} value={lv}>
-                            Lv{lv}
-                          </MenuItem>
-                        ))}
-                      </Select>
-                    </div>
+                        <SeaIcon icon="fluent:delete-24-filled" />
+                      </IconButton>
+                    </Paper>
+                  ))}
+                {e.assetRequirement.length > 0 &&
+                  e.assetRequirement.map((asset, i2) => (
+                    <Paper key={i2} className="sea-paper role-one" elevation={12}>
+                      <div className="left">
+                        <h4>Asset requirement</h4>
+                        <h5>Contract address</h5>
+                        <div className="sea-operation-box">
+                          <Select size="small" defaultValue={asset.chain} disabled>
+                            <MenuItem value={Chain.eth}>Ethereum</MenuItem>
+                          </Select>
+                          <TextField
+                            size="small"
+                            defaultValue={asset.address}
+                            onChange={(event) => (asset.address = event.target.value)}
+                            variant="outlined"
+                            placeholder="Contract address"
+                          />
+                          <Button onClick={() => message.warn('NFT 弹窗，正在开发')}>NFT</Button>
+                        </div>
+                        <h5>Amount</h5>
+                        <div className="sea-operation-box">
+                          <Select size="small" defaultValue="Amount" disabled>
+                            <MenuItem value="Amount">Amount</MenuItem>
+                          </Select>
+                          <Select size="small" defaultValue={asset.range}>
+                            <MenuItem value={0}>{'≥'}</MenuItem>
+                            <MenuItem value={1}>{'≤'}</MenuItem>
+                            <MenuItem value={2}>{'='}</MenuItem>
+                          </Select>
+                          <TextField
+                            size="small"
+                            defaultValue={asset.amount}
+                            onChange={(event) => (asset.amount = Number(event.target.value))}
+                            type="number"
+                          />
+                        </div>
+                      </div>
+                      <IconButton
+                        className="right delete"
+                        aria-label="delete"
+                        color="error"
+                        onClick={() => bindDel('asset', i, i2)}
+                      >
+                        <SeaIcon icon="fluent:delete-24-filled" />
+                      </IconButton>
+                    </Paper>
+                  ))}
+                <div className="new-requirement">
+                  <h4>Add a new requirement</h4>
+                  <div className="sea-operation-box">
+                    <Button variant="outlined" onClick={() => bindAdd('unipass', i)}>
+                      UniPass requirement
+                    </Button>
+                    <Button variant="contained" onClick={() => bindAdd('asset', i)}>
+                      Asset requirement
+                    </Button>
                   </div>
-                  <IconButton
-                    className="right delete"
-                    aria-label="delete"
-                    color="error"
-                    onClick={() => bindDel('unipass', i, i2)}
+                </div>
+                <div className="save">
+                  <Button
+                    className="submit"
+                    variant="contained"
+                    color="secondary"
+                    onClick={() => {
+                      bindSave(e)
+                    }}
                   >
-                    <SeaIcon icon="fluent:delete-24-filled" />
-                  </IconButton>
-                </Paper>
-              ))}
-            {e.assetRequirement.length > 0 &&
-              e.assetRequirement.map((asset, i2) => (
-                <Paper key={i2} className="sea-paper role-one" elevation={12}>
-                  <div className="left">
-                    <h4>Asset requirement</h4>
-                    <h5>Contract address</h5>
-                    <div className="sea-operation-box">
-                      <Select size="small" defaultValue={asset.chain} disabled>
-                        <MenuItem value={Chain.eth}>Ethereum</MenuItem>
-                      </Select>
-                      <TextField
-                        size="small"
-                        defaultValue={asset.address}
-                        onChange={(event) => (asset.address = event.target.value)}
-                        variant="outlined"
-                        placeholder="Contract address"
-                      />
-                      <Button onClick={() => message.warn('NFT 弹窗，正在开发')}>NFT</Button>
-                    </div>
-                    <h5>Amount</h5>
-                    <div className="sea-operation-box">
-                      <Select size="small" defaultValue="Amount" disabled>
-                        <MenuItem value="Amount">Amount</MenuItem>
-                      </Select>
-                      <Select size="small" defaultValue={asset.range}>
-                        <MenuItem value={0}>{'≥'}</MenuItem>
-                        <MenuItem value={1}>{'≤'}</MenuItem>
-                        <MenuItem value={2}>{'='}</MenuItem>
-                      </Select>
-                      <TextField
-                        size="small"
-                        defaultValue={asset.amount}
-                        onChange={(event) => (asset.amount = Number(event.target.value))}
-                        type="number"
-                      />
-                    </div>
-                  </div>
-                  <IconButton
-                    className="right delete"
-                    aria-label="delete"
-                    color="error"
-                    onClick={() => bindDel('asset', i, i2)}
-                  >
-                    <SeaIcon icon="fluent:delete-24-filled" />
-                  </IconButton>
-                </Paper>
-              ))}
-            <div className="new-requirement">
-              <h4>Add a new requirement</h4>
-              <div className="sea-operation-box">
-                <Button variant="outlined" onClick={() => bindAdd('unipass', i)}>
-                  UniPass requirement
-                </Button>
-                <Button variant="contained" onClick={() => bindAdd('asset', i)}>
-                  Asset requirement
-                </Button>
-              </div>
-            </div>
-            <div className="save">
-              <Button
-                className="submit"
-                variant="contained"
-                color="secondary"
-                onClick={() => {
-                  bindSave(e)
-                }}
-              >
-                Save
-              </Button>
-            </div>
-          </div>
+                    Save
+                  </Button>
+                </div>
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
         )
       })}
       <div className="sea-new-box">
