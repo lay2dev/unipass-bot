cb7c7a17abc2c5c8efe1758795494e8365ac556d
大海团队 <mp@sea.team>
2022-03-09 14:35:35 +0800

    page-channel list

--- pages/channel-manage/index.tsx
+++ pages/channel-manage/index.tsx
@@ -14,15 +14,17 @@ import {
   AccordionDetails,
 } from '@mui/material'
 import api from '../../assets/js/api'
+import mock from './mock'
 
 const Page: NextPage = () => {
   const [state] = useStore()
-  const [roles, setRoles] = useState([])
-  useEffect(() => {
-    api.get('/channels/' + state.server).then((res) => {
-      console.log('🌊', res)
-    })
-  }, [state.server])
+  const [channels, setChannels] = useState(mock)
+  // useEffect(() => {
+  //   api.get('/channels/' + state.server).then((res) => {
+  //     console.log('channels', res.data)
+  //     setChannels(res.data)
+  //   })
+  // }, [state.server])
 
   const bindSwitch = (event: React.ChangeEvent<HTMLInputElement>) => {
     console.log('🌊', event.target.checked)
@@ -51,90 +53,102 @@ const Page: NextPage = () => {
       lv: 'UP Lv2',
     },
   ])
+  const bindRuleDel = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>, i: number) => {
+    event.stopPropagation()
+  }
   return (
     <div id="page-channel-manage">
-      <Accordion className="sea-box-one">
-        <AccordionSummary
-          expandIcon={<SeaIcon sx={{ fontSize: '32px' }} icon="ic:round-expand-more" />}
-        >
-          <div className="info">
-            <div className="title">
-              <div className="sea-h3">Channel</div>
-              <SeaChannel name="UniPass" />
-              <SeaSwitch
-                className="switch"
-                onClick={(event) => {
-                  event.stopPropagation()
-                }}
-                onChange={bindSwitch}
-                defaultChecked={true}
-              />
-            </div>
-            <div className="tip-box">
-              <div className="one">
-                <SeaIcon icon="icon-park-outline:message-one"></SeaIcon>
-                <SeaRole color="#c4505e" text="UP Lv4" />
-                <SeaRole color="#e9c0a0" text="UP Lv3" />
+      {channels.map((e, i) => (
+        <Accordion key={i} className="sea-box-one">
+          <AccordionSummary
+            expandIcon={<SeaIcon sx={{ fontSize: '32px' }} icon="ic:round-expand-more" />}
+          >
+            <div className="info">
+              <div className="title">
+                <div className="sea-h3">Channel</div>
+                <SeaChannel name={e.name} />
+                <IconButton
+                  className="delete"
+                  color="error"
+                  onClick={(event) => bindRuleDel(event, i)}
+                >
+                  <SeaIcon icon="fluent:delete-24-filled" />
+                </IconButton>
+                <SeaSwitch
+                  className="switch"
+                  onClick={(event) => {
+                    event.stopPropagation()
+                  }}
+                  onChange={bindSwitch}
+                  defaultChecked={true}
+                />
               </div>
-              <div className="one">
-                <SeaIcon icon="icon-park-outline:message-privacy"></SeaIcon>
-                <SeaRole color="#4fab9f" text="UP Lv1" />
-                <SeaRole color="#3b7669" text="UP Lv2" />
-              </div>
-            </div>
-          </div>
-        </AccordionSummary>
-        <AccordionDetails>
-          <div className="sea-h3">Set Channel</div>
-          <Select className="select" size="small" value={channel} onChange={bindChannel}>
-            <MenuItem value="UniPass"># UniPass</MenuItem>
-            <MenuItem value="Aven"># Aven</MenuItem>
-          </Select>
-          <div className="sea-h3">Message manage</div>
-          <Paper className="sea-paper" elevation={12}>
-            <div className="view-channel">
-              <h4>View channel</h4>
-              <h5>Allows members to view this channel.</h5>
-              <div className="sea-operation-box">
-                {viewChannel.map((e, i) => {
-                  return <SeaRole key={i} color={e.color} text={e.lv} />
-                })}
-                <div className="operation">
-                  <IconButton>
-                    <SeaIcon icon="fluent:add-circle-16-regular"></SeaIcon>
-                  </IconButton>
-                  <IconButton>
-                    <SeaIcon icon="fluent:subtract-circle-16-regular"></SeaIcon>
-                  </IconButton>
+              <div className="tip-box">
+                <div className="one">
+                  <SeaIcon icon="icon-park-outline:message-one"></SeaIcon>
+                  <SeaRole color="#c4505e" text="UP Lv4" />
+                  <SeaRole color="#e9c0a0" text="UP Lv3" />
+                </div>
+                <div className="one">
+                  <SeaIcon icon="icon-park-outline:message-privacy"></SeaIcon>
+                  <SeaRole color="#4fab9f" text="UP Lv1" />
+                  <SeaRole color="#3b7669" text="UP Lv2" />
                 </div>
               </div>
             </div>
-          </Paper>
-          <Paper className="sea-paper" elevation={12}>
-            <div className="send-message">
-              <h4>Send message</h4>
-              <h5>Allow those members to publish their own message.</h5>
-              <div className="sea-operation-box">
-                <SeaRole color="#4fab9f" text="UP Lv1" />
-                <SeaRole color="#3b7669" text="UP Lv2" />
-                <div className="operation">
-                  <IconButton>
-                    <SeaIcon icon="fluent:add-circle-16-regular"></SeaIcon>
-                  </IconButton>
-                  <IconButton>
-                    <SeaIcon icon="fluent:subtract-circle-16-regular"></SeaIcon>
-                  </IconButton>
+          </AccordionSummary>
+          <AccordionDetails>
+            <div className="sea-h3">Set Channel</div>
+            <Select className="select" size="small" value={channel} onChange={bindChannel}>
+              <MenuItem value="UniPass"># UniPass</MenuItem>
+              <MenuItem value="Aven"># Aven</MenuItem>
+            </Select>
+            <div className="sea-h3">Message manage</div>
+            <Paper className="sea-paper" elevation={12}>
+              <div className="view-channel">
+                <h4>View channel</h4>
+                <h5>Allows members to view this channel.</h5>
+                <div className="sea-operation-box">
+                  {viewChannel.map((e, i) => {
+                    return <SeaRole key={i} color={e.color} text={e.lv} />
+                  })}
+                  <div className="operation">
+                    <IconButton>
+                      <SeaIcon icon="fluent:add-circle-16-regular"></SeaIcon>
+                    </IconButton>
+                    <IconButton>
+                      <SeaIcon icon="fluent:subtract-circle-16-regular"></SeaIcon>
+                    </IconButton>
+                  </div>
                 </div>
               </div>
+            </Paper>
+            <Paper className="sea-paper" elevation={12}>
+              <div className="send-message">
+                <h4>Send message</h4>
+                <h5>Allow those members to publish their own message.</h5>
+                <div className="sea-operation-box">
+                  <SeaRole color="#4fab9f" text="UP Lv1" />
+                  <SeaRole color="#3b7669" text="UP Lv2" />
+                  <div className="operation">
+                    <IconButton>
+                      <SeaIcon icon="fluent:add-circle-16-regular"></SeaIcon>
+                    </IconButton>
+                    <IconButton>
+                      <SeaIcon icon="fluent:subtract-circle-16-regular"></SeaIcon>
+                    </IconButton>
+                  </div>
+                </div>
+              </div>
+            </Paper>
+            <div className="save">
+              <Button className="submit" variant="contained" color="secondary">
+                Save
+              </Button>
             </div>
-          </Paper>
-          <div className="save">
-            <Button className="submit" variant="contained" color="secondary">
-              Save
-            </Button>
-          </div>
-        </AccordionDetails>
-      </Accordion>
+          </AccordionDetails>
+        </Accordion>
+      ))}
     </div>
   )
 }
