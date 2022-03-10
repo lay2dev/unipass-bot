4eca11427227bfb142ebe503398fe33bb9e747de
大海团队 <mp@sea.team>
2022-03-10 16:53:38 +0800

    remove Chinese notes

--- assets/js/api.ts
+++ assets/js/api.ts
@@ -1,4 +1,3 @@
-// import { ElMessage } from 'element-plus'
 import axios from 'axios'
 const api = axios.create({
   baseURL: 'https://t.api.unipass.id/admin',
@@ -16,29 +15,25 @@ declare module 'axios' {
   }
 }
 
-// 添加请求拦截器 https://axios-http.com/zh/docs/interceptors
+// https://axios-http.com/zh/docs/interceptors
 api.interceptors.request.use(
   function (config) {
-    // 在发送请求之前做些什么
     return config
   },
   function (error) {
-    // 对请求错误做些什么
     return error
   },
 )
 
-// 添加响应拦截器
+// https://axios-http.com/zh/docs/interceptors
 api.interceptors.response.use(
   function (response) {
-    // 2xx 范围内的状态码都会触发该函数。
-    // 对响应数据做点什么
     return response.data
   },
   function (error) {
     const code = error.response.status
     if (code === 401) {
-      // token 过期
+      // token Expired
       const { authorization: accessToken, refreshtoken: refreshToken } = error.response.headers
       api.defaults.headers.common['Authorization'] = 'Bearer ' + accessToken
       api.defaults.headers.common['refreshToken'] = refreshToken
@@ -56,9 +51,6 @@ api.interceptors.response.use(
       window.localStorage.removeItem('UP-BOT')
       window.location.href = '/login'
     }
-    // 超出 2xx 范围的状态码都会触发该函数。
-    // 对响应错误做点什么
-    // ElMessage.error('请求失败')
     return error
   },
 )
